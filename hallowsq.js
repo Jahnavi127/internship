5e91cf7ad51496c3c02963a6e889e89e2d7acf20
Andhavarapu Jahnavi <janujasu127@gmail.com>
2023-12-13 15:47:39 +0530

    commiting changes

---
+++ hallowsq.js
@@ -0,0 +1,13 @@
+let i,j,n;
+n=5;
+for(i=0;i<n;i++){
+    for(j=0;j<n;j++){
+        if(i==0 || j==0 || i==n-1 || j==n-1){
+            process.stdout.write("*");
+        }
+        else{
+            process.stdout.write(" "); 
+        }
+    }
+    console.log();
+}
